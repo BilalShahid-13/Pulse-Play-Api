2e4cb476ca3da4a2ead7b26198a4babbeecb4e1c
Bilal Shahid <bcsm-f21-249@superior.edu.pk>
2024-08-23 02:56:07 +0500

    change method of artist route

--- src/routes/artist.route.js
+++ src/routes/artist.route.js
@@ -3,6 +3,6 @@ import { fetchArtist } from "../controllers/Artist.controller.js";
 
 const artistRouter = Router();
 
-artistRouter.post("/fetchSongs", fetchArtist);
+artistRouter.get("/fetchSongs", fetchArtist);
 
 export { artistRouter };
